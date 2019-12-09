63b83f60597abce0a610aaa52522fa9957bdd4ff
André <Andr@192.168.1.66>
2019-12-09 20:34:51 +0100

    adfs

--- suduko-projekt/src/SudukoInterface.java
+++ suduko-projekt/src/SudukoInterface.java
@@ -6,13 +6,12 @@ public class SudukoInterface {
 	// wtf am i thingking abotui iwjis wit wiejij
 
 	//What up squadfam exxxxdddd
+
+	public void newMethod() {
+		
+		
+	}
 	
-	//I make changes to test pulling down shit :p
-	//What up squadfam exxxxdddd
-	
-	//I make changes to test pulling down shit :p
-	//What up squadfam exxxxdddd//I make changes to test pulling down shit :p
-	//What up squadfam exxxxdddd
 	//I make changes to test pulling down shit :p
 	//What up squadfam exxxxdddd
 	//I make changes to test pulling down shit :p
@@ -20,5 +19,6 @@ public class SudukoInterface {
 	//What up squadfam exxxxdddd
 	//I make changes to test pulling down shit :p
 	//What up squadfam exxxxddddv
+
 
 }
