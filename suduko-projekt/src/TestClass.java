17a9aaf2a8bf8361d9d73b3f7ea0f3755ecf3a0b
André <Andr@192.168.1.66>
2019-12-09 21:36:17 +0100

    tagit bort test grejs och fixat lite mall

---
+++ suduko-projekt/src/TestClass.java
@@ -0,0 +1,11 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+
+		SudukoSolver sSsolver = new SudukoSolver();
+		SudukoInterface sInterface = new SudukoInterface();
+
+	}
+
+}
